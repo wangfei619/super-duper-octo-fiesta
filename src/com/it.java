bdb3b24bf7297d63d5094cf6f3a7076d6918bd23
wangfeifan <145@>
2023-03-27 13:19:24 +0800

    11

--- src/com/it.java
+++ src/com/it.java
@@ -4,5 +4,13 @@ public class it {
 
     public static void main(String[] args) {
         System.out.println("原始项目");
+        System.out.println("原始项目2");
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
     }
 }
